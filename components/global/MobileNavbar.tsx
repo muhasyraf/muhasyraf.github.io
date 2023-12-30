ee391438c3512ebe56419448dbaad33b6c1945c5
muhasyraf <faizkamil39@gmail.com>
2023-12-30 10:02:25 +0700

    fix responsive navbar

--- components/global/MobileNavbar.tsx
+++ components/global/MobileNavbar.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 import Link from "next/link";
-import {routes} from "@/data/global";
+import { routes } from "@/data/global";
 import useDelayedRender from "use-delayed-render";
 
 export default function MobileNavbar() {
@@ -33,15 +33,17 @@ export default function MobileNavbar() {
   return (
     <nav>
       <div
-        className={`w-full justify-between flex items-center ${isMenuRendered && 'bg-bg'} p-5`}
+        className={`w-full justify-between flex items-center ${
+          isMenuRendered && "bg-bg"
+        } p-5`}
         style={{ zIndex: 101 }}
       >
         <li className="list-none font-bold text-lg">
           <Link href="/">
             <img
-              className="mr-3"
-              src="/static/logos/logo_full.svg"
-              width="160"
+              className="mr-3 rounded-full"
+              src="/static/img/linkedinprofilecrop.png"
+              width="65"
             />
           </Link>
         </li>
