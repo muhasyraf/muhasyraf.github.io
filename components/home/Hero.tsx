4a6022bade0434ba91bcab17af1b76aa85cce9a6
muhasyraf <faizkamil39@gmail.com>
2024-01-15 11:32:31 +0700

    add floating icon on hero section

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -8,11 +8,11 @@ function Hero() {
         className="relative heroElem w-full pt-20 pb-40 mx-auto mt-10 flex justify-center text-center flex-col items-center z-1"
         style={{ maxWidth: "1200px" }}
       >
-        <p className="text-xl mb-5 font-light">
+        <p className="text-xl mb-5 font-light z-10">
           Muhammad <span className="font-medium text-fun-pink">Asyraf</span>{" "}
           Faiz Kamil
         </p>
-        <h1 className="heroTitle inline-block max-w-2xl lg:max-w-4xl  w-auto relative text-5xl md:text-6xl lg:text-7xl tracking-tighter mb-10 font-bold heroShinyBg">
+        <h1 className="heroTitle inline-block max-w-2xl lg:max-w-4xl  w-auto relative text-5xl md:text-6xl lg:text-7xl tracking-tighter mb-10 font-bold heroShinyBg z-10">
           a Grateful{" "}
           <span className="heroShiny1 text-fun-pink">Lifelong Learner</span>{" "}
         </h1>
@@ -23,6 +23,7 @@ function Hero() {
           offset={-30}
           smooth={true}
           duration={500}
+          className="z-10"
         >
           <div className="group cursor-pointer font-bold whitespace-nowrap px-4 py-2 text-fun-white border-2 text-2xl rounded-3xl border-fun-pink bg-transparent hover:bg-fun-pink hover:text-bg hover:border-fun-pink transition-colors flex flex-row space-x-4 items-center">
             <div>
@@ -40,6 +41,30 @@ function Hero() {
             </svg>
           </div>
         </ScrollLink>
+        <img
+          className="sqD top-[0px] left-2 sm:left-40"
+          style={{ animationDelay: "1s" }}
+          src="/static/sqd/laravel.svg"
+          alt="laravel"
+        />
+        <img
+          className="sqD top-20 sm:top-[40px] right-[5%] sm:right-[170px]"
+          style={{ animationDelay: "0.1s" }}
+          src="/static/sqd/react.svg"
+          alt="react"
+        />
+        <img
+          className="sqD bottom-2 sm:top-60 left-4 sm:left-72"
+          style={{ animationDelay: "0.5s" }}
+          src="/static/sqd/code.svg"
+          alt="code"
+        />
+        <img
+          className="sqD bottom-14 sm:top-72 right-16 sm:right-[295px]"
+          style={{ animationDelay: "1s" }}
+          src="/static/sqd/github.svg"
+          alt="github"
+        />
       </div>
     </>
   );
